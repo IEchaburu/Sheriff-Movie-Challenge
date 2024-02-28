8ed2ee3f040eef0059348a04b5b182b7d2fd2b24
IEchaburu <inaki.echaburu@hotmail.com>
2024-02-28 13:18:27 -0300

    redux de movies terminado

--- client/src/Redux/Actions/Movies/movieActions.js
+++ client/src/Redux/Actions/Movies/movieActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_POPULAR_MOVIES } from "./movieActionTypes";
+import { GET_POPULAR_MOVIES, GET_TOP_RATED_MOVIES, GET_MOVIES_NAME, GET_MOVIE_ID, GET_MOVIES_GENRES } from "./movieActionTypes";
 
 export const getPopularMovies = () => {
     return async (dispatch) => {
@@ -8,6 +8,70 @@ export const getPopularMovies = () => {
 
             return dispatch({
                 type: GET_POPULAR_MOVIES,
+                payload: data
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+export const getTopRateMovies = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get("http://localhost:3001/movie/top_rated_movies")
+
+            return dispatch({
+                type: GET_TOP_RATED_MOVIES,
+                payload: data
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+};
+
+export const getMoviesByName = (name) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get(`http://localhost:3001/movie/name?name=${name}`)
+
+            return dispatch({
+                type: GET_MOVIES_NAME,
+                payload: data
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+}
+
+export const getMovieId = (id) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get(`http://localhost:3001/movie/${id}`)
+
+            return dispatch({
+                type: GET_MOVIE_ID,
+                payload: data
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+}
+
+export const getMovieGenres = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get("http://localhost:3001/movie_genres")
+
+            return dispatch({
+                type: GET_MOVIES_GENRES,
                 payload: data
             });
 
--- client/src/Redux/Reducer/reducer.js
+++ client/src/Redux/Reducer/reducer.js
@@ -1,17 +1,49 @@
-import { GET_POPULAR_MOVIES } from "../Actions/Movies/movieActionTypes";
+import { GET_MOVIES_GENRES, GET_MOVIES_NAME, GET_MOVIE_ID, GET_POPULAR_MOVIES, GET_TOP_RATED_MOVIES } from "../Actions/Movies/movieActionTypes";
 
 const initialState = {
-    popularMovies: []
+    //ESTADOS RELACIONADOS A LAS MOVIES
+    popularMovies: [],
+    topRatedMovies: [],
+    moviesByName: [],
+    movieByID: [],
+    movieGenres: []
 };
 
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
+
+        //CASOS RELACIONADOS A LAS MOVIES
         case GET_POPULAR_MOVIES:
             return {
                 ...state,
                 popularMovies: action.payload
             };
+        case GET_TOP_RATED_MOVIES:
+            return {
+                ...state,
+                topRatedMovies: action.payload
+            };
+        case GET_MOVIES_NAME:
+            if (action.payload.length === 0) {
+                window.alert("The movie wasn't found")
+            }
+            return {
+                ...state,
+                moviesByName: action.payload,
+            };
+        case GET_MOVIE_ID:
+            return {
+                ...state,
+                movieByID: action.payload
+            };
+        case GET_MOVIES_GENRES:
+            return {
+                ...state,
+                movieGenres: action.payload
+            };
+
+        //CASOS RELACIONADOS A LAS SERIES
             
         default:
             return state;
