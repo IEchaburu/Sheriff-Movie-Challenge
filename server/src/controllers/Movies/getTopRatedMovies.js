75b71de5582742c64fd7423aa799d4d7a378c4e0
IEchaburu <inaki.echaburu@hotmail.com>
2024-02-27 21:32:44 -0300

    while agregado en el get de movies y series para peticionar elementos de mas paginas

--- server/src/controllers/Movies/getTopRatedMovies.js
+++ server/src/controllers/Movies/getTopRatedMovies.js
@@ -6,24 +6,31 @@ const { API_KEY } = process.env;
 
 const getTopRatedMovies = async () => {
     const apiMovies = [];
+    let index = 1
 
-    //LE PIDE LAS PELICULAS A LA API
-    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`);
-
-    //RECORRE EL RESULTADO Y AGRUPA LAS PELICULAS EN EL ARRAY apiMovies
-    if (data.results) {
-        data.results.forEach(element => {
-            const movie = {
-                id: element.id,
-                name: element.title,
-                image: element.poster_path,
-                genreIds: element.genre_ids,
-                release: element.release_date,
-                rating: element.vote_average
-            };
-            apiMovies.push(movie);
-        });
-    };
+    //CON EL WHILE SE HACEN PETICIONES A LA CANTIDAD DE PAGINAS INDICADAS PARA ASI
+    //PODER JUNTAR LA CANTIDAD DE PELICULAS DESEADAS, EN ESTE CASO: 100    
+    while (index < 6) {
+        
+        //LE PIDE LAS PELICULAS A LA API
+        const { data } = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${index}&api_key=${API_KEY}`);
+    
+        //RECORRE EL RESULTADO Y AGRUPA LAS PELICULAS EN EL ARRAY apiMovies
+        if (data.results) {
+            data.results.forEach(element => {
+                const movie = {
+                    id: element.id,
+                    name: element.title,
+                    image: element.poster_path,
+                    genreIds: element.genre_ids,
+                    release: element.release_date,
+                    rating: element.vote_average
+                };
+                apiMovies.push(movie);
+            });
+        };
+        index++;
+    }
 
     return apiMovies;
 };
--- server/src/controllers/Series/getTopRatedSeries.js
+++ server/src/controllers/Series/getTopRatedSeries.js
@@ -6,24 +6,32 @@ const { API_KEY } = process.env;
 
 const getTopRatedSeries = async () => {
     const apiSeries = [];
+    let index = 1;
 
-    //LE PIDE LAS SERIES A LA API
-    const { data } = await axios.get(`https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1&api_key=${API_KEY}`);
+    //CON EL WHILE SE HACEN PETICIONES A LA CANTIDAD DE PAGINAS INDICADAS PARA ASI
+    //PODER JUNTAR LA CANTIDAD DE SERIES DESEADAS, EN ESTE CASO: 100    
+    while (index < 6) {
+        
+        //LE PIDE LAS SERIES A LA API
+        const { data } = await axios.get(`https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=${index}&api_key=${API_KEY}`);
+    
+        //RECORRE EL RESULTADO Y AGRUPA LAS SERIES EN EL ARRAY apiSeries
+        if (data.results) {
+            data.results.forEach(element => {
+                const serie = {
+                    id: element.id,
+                    name: element.name,
+                    image: element.poster_path,
+                    genreIds: element.genre_ids,
+                    release: element.first_air_date,
+                    rating: element.vote_average
+                };
+                apiSeries.push(serie);
+            });
+        };
+        index++;
+    }
 
-    //RECORRE EL RESULTADO Y AGRUPA LAS SERIES EN EL ARRAY apiSeries
-    if (data.results) {
-        data.results.forEach(element => {
-            const serie = {
-                id: element.id,
-                name: element.name,
-                image: element.poster_path,
-                genreIds: element.genre_ids,
-                release: element.first_air_date,
-                rating: element.vote_average
-            };
-            apiSeries.push(serie);
-        });
-    };
 
     return apiSeries;
 };
